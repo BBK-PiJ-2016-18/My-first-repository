49e9ad3de61e77787296922d6f98d5388654cb33
BBK-PiJ-2016-18 <phewso01@dcs.bbk.ac.uk>
2016-10-10 15:01:24 +0100

    Add files via upload

---
+++ e3.groovy
@@ -0,0 +1,5 @@
+print "Enter the calculation you whish to perform: "
+String s = System.console().readLine()
+println args
+println args[0]
+println args.length()
---
+++ e4.groovy
@@ -0,0 +1,33 @@
+class Point {
+double x;
+double y;
+}
+print "Enter the x-coordinate of the first point: "
+double x1 = Double.parseDouble(System.console().readLine())
+print "Enter the y-coordinate of the first point: "
+double y1 = Double.parseDouble(System.console().readLine())
+print "Enter the x-coordinate of the second point: "
+double x2 = Double.parseDouble(System.console().readLine())
+print "Enter the y-coordinate of the second point: "
+double y2 = Double.parseDouble(System.console().readLine())
+print "Enter the x-coordinate of the third point: "
+double x3 = Double.parseDouble(System.console().readLine())
+print "Enter the y-coordinate of the third point: "
+double y3 = Double.parseDouble(System.console().readLine())
+double p1p2_dist = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+double p1p3_dist = (x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3)
+double p2p3_dist = (x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3)
+if (p1p2_dist < p1p3_dist) {
+	if (p1p2_dist < p2p3_dist) {
+		println "point 1 (" + x1 + ", " + y1 + ") and point 2 (" + x2 + ", " + y2 + ") are the closest"
+	}
+	else {
+		println "point 2 (" + x2 + ", " + y2 + ") and point 3 (" + x3 + ", " + y3 + ") are the closest"
+	}
+}
+else if (p1p3_dist < p2p3_dist) {
+	println "point 1 (" + x1 + ", " + y1 + ") and point 3 (" + x3 + ", " + y3 + ") are the closest"
+}
+else {
+	println "point 2 (" + x2 + ", " + y2 + ") and point 3 (" + x3 + ", " + y3 + ") are the closest"
+}
