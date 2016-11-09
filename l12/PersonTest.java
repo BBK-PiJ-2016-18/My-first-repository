d5108a4a13e08d62ec75c2b1ad37cebf9b6c3660
Patrick Hewson <phewso01@dcs.bbk.ac.uk>
2016-11-09 17:21:47 +0000

    Added more tests before algorithm

--- l12/PersonTest.java
+++ l12/PersonTest.java
@@ -5,9 +5,14 @@ public class PersonTest {
 	@Test
 	public void testsNormalName() {
 		Person p = new Person();
-		String input = "Donald  John  Trump";
-		String output = p.getInitials(input);
-		String expected = "DJT";
-		assertEquals(output, expected);
+		String input1 = "Dereck Robert Yssirt";
+		String output1 = p.getInitials(input1);
+		String expected1 = "DRY";
+		assertEquals(output1, expected1);
+		Person q = new Person();
+		String input2 = "Donald  John  Trump";
+		String output2 = q.getInitials(input2);
+		String expected2 = "DJT";
+		assertEquals(output2, expected2);
 	}
 }
