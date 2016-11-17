ee16b623740e97ec06b99066d9ca883cad205caa
Patrick Hewson <Patrick Hewson>
2016-11-17 18:46:39 +0000

    Reorganizing

--- l6/9marks/Mark.java
+++ l6/9marks/Mark.java
@@ -1,24 +1,8 @@
 class Mark {
 	private int mark;
 	private String grade;
-}
-
-public int getMark() {
-	return this.mark;
-}
-
-public String getGrade(Mark thismark) {
-	if (thismark.getMark() > 70) {
-			mark.grade = "Distinction";
-		}
-		else if (thismark.getMark() > 50) {
-			thismark.grade = "Pass";
-		}
-		else if (thismark.getMark() > 0) {
-			thismark.grade = "Fail";
-		}
-		else {
-			thismark.grade = "Invalid";
-		}
+	
+	public void assignGrade() {
+		if
 	}
 }
