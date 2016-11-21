ade1d50f5279882b63f36a215131a8310f5592b7
Patrick Hewson <phewso01@dcs.bbk.ac.uk>
2016-11-21 14:51:12 +0000

    Added day 15

---
+++ l15/1codeflow/Launcher.java
@@ -0,0 +1,13 @@
+public class Launcher {
+	public static void main(String[] args) {
+		IntList myList = new IntList();
+		System.out.println("userIntput is 0:");
+		myList.launch(0);
+		System.out.println("userIntput is 2:");
+		myList.launch(2);
+		System.out.println("userIntput is 4:");
+		myList.launch(4);
+		System.out.println("userIntput is 6:");
+		myList.launch(6);
+	}
+}
