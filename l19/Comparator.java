93a24ef067ff706dc1b42260aaff95216e0153a4
Patrick Hewson <phewso01@dcs.bbk.ac.uk>
2016-12-05 16:56:05 +0000

    update

--- l19/Comparator.java
+++ l19/Comparator.java
@@ -1,10 +1,18 @@
-import java.util.Arrays
+import java.util.Arrays;
 
 public class Comparator {
-		private Array array;
+	
+	Comparator(Array list) {
+		Array ordered = new Array();
+	}
 		
-		public int compare(T o1, T o2) {
-			
+	public int compare(int n1, int n2) {
+		if (n1 < n2) {
+			return -1;
 		}
+		else if (n1 == n2) {
+			return 0;
+		}
+		return 1;
 	}
 }
--- l19/Launcher.java
+++ l19/Launcher.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 
 public class Launcher{
 	public static void main(String[] args) {
-		Integer[] intArray = {1,55,12,14,32,22,99,3};
-		System.out.println(Arrays.asList(intArray));
+		//Integer[] intArray = {1,55,12,14,32,22,99,3};
+		String[] stringArray = {"string", "strong", "abc", "77", "1234567890"};
+		System.out.println(Arrays.asList(stringArray));
 	}
-}	
+}
