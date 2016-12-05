55a634cae410b9186f609f0ae563070b0a9c98da
Patrick Hewson <phewso01@dcs.bbk.ac.uk>
2016-12-05 16:37:51 +0000

    Added day 19

---
+++ l19/Comparator.java
@@ -0,0 +1,10 @@
+import java.util.Arrays
+
+public class Comparator {
+		private Array array;
+		
+		public int compare(T o1, T o2) {
+			
+		}
+	}
+}
---
+++ l19/Launcher.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Launcher{
+	public static void main(String[] args) {
+		Integer[] intArray = {1,55,12,14,32,22,99,3};
+		System.out.println(Arrays.asList(intArray));
+	}
+}	
