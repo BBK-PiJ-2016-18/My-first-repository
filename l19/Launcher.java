c53d9cfe46c3407d6e0ccc4d454bd715edc8edaf
Patrick Hewson <phewso01@dcs.bbk.ac.uk>
2016-12-07 15:38:44 +0000

    Update

--- l19/Launcher.java
+++ l19/Launcher.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
 
-public class Launcher{
+public class Launcher {
 	public static void main(String[] args) {
 		//Integer[] intArray = {1,55,12,14,32,22,99,3};
 		String[] stringArray = {"string", "strong", "abc", "77", "1234567890"};
 		System.out.println(Arrays.asList(stringArray));
+		Compataror comparator = new comparator
 	}
 }
--- l19/StringCompatatorByLength.java
+++ l19/StringCompatatorByLength.java
@@ -1,4 +1,5 @@
-// copied from what Sergio wrote on board, compare to much shortler but equivalent code w/ lambdas in Keith's eg
+// copied from what Sergio wrote on board, compare to much shorter but equivalent code w/ lambdas in Keith's video (or BBK-PiJ-2016-00?) eg
+// Array.sort( words, (s1, s2) _> ...
 
 public class StringComparatorByLength implements Comparator<String> {
 	@Override
