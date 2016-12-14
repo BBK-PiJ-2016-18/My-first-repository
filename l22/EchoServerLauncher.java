5d2a333a40e3629875d81b6f7307520b6007eec6
Patrick Hewson <phewso01@dcs.bbk.ac.uk>
2016-12-14 14:20:50 +0000

    added day 22

---
+++ l22/EchoServerLauncher.java
@@ -0,0 +1,20 @@
+public class EchoServerLauncher {
+	private void launch() {
+		try {
+			// 1. Create the registry if there is not one
+			LocateRegistry.createRegistry(1099);
+			// 2. Create the server object
+			EchoServer server = new EchoServer();
+			// 3. Register (bind) the server object on the registy.
+			// The registry may be on a different machine
+			String registryAddress = "localhost";
+			String registryUrl = "//" + registryAddress + "/";
+			String serviceName = "echo";
+			Naming.rebind(registryUrl + serviceName, server);
+		} catch (MalformedURLException ex) {
+			ex.printStackTrace();
+		} catch (RemoteException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
