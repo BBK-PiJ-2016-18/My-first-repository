4db6177f60243753196c1f8f27e88658ae0971db
Patrick Hewson <Patrick Hewson>
2017-01-12 18:51:11 +0000

    hopefully everything actually fixed now!

--- test.groovy
+++
@@ -1,9 +0,0 @@
-	print "Please enter a number between 1 and 25: "
-String s = System.console().readLine()
-int n = Integer.parseInt(s)
-if (n > 25) or (n < 0) {
-	println "That is not valid"
-}
-if (n < 25) and (n > 0) {
-	println "That is valid"
-}
